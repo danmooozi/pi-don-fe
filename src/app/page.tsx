0160fdf71d043f7f43c313b376aeb3b007816937
김수빈 <57705512+MOBUMIN@users.noreply.github.com>
2023-12-12 22:41:42 +0900

    feat(Button): button 호버 효과 추가
    * vscode 세팅
    * prettier, eslint 기존 파일에 적용
    * pretendard 잠시 빼기(오류남)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,53 @@
-import Image from 'next/image'
-import styles from './page.module.scss';
-import Button from '@/components/Button';
+import Image from "next/image";
+import styles from "./page.module.scss";
+import Button from "@/components/Button";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.main__background}>
-        <Image src="/index_lines.svg"
-                alt="background"
-                width={0}
-                height={0}
-                sizes='100vw'
-                style={{ width: '100%', height: 'auto' }} />
+        <Image
+          src="/index_lines.svg"
+          alt="background"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "100%", height: "auto" }}
+        />
         <Button>깃허브 로그인하기</Button>
-        
       </div>
       <div className={styles.main__body}>
         <div className={styles.main__title}>
           <span className={styles.main__title__year}>
-            <Image src="/index_title.svg"
-                  alt="title"
-                  width={265}
-                  height={0}
-                  style={{ height: 'auto' }} />
-            <Image src="/index_0line.svg"
-                  alt="line"
-                  width={43}
-                  height={0}
-                  style={{ height: 'auto', position: 'absolute', top: '20px', left: '80px' }} />
+            <Image
+              src="/index_title.svg"
+              alt="title"
+              width={265}
+              height={0}
+              style={{ height: "auto" }}
+            />
+            <Image
+              src="/index_0line.svg"
+              alt="line"
+              width={43}
+              height={0}
+              style={{
+                height: "auto",
+                position: "absolute",
+                top: "20px",
+                left: "80px",
+              }}
+            />
           </span>
-          <p>
-            엠비티아이
-          </p>
+          <p>엠비티아이</p>
         </div>
-        <Image src="/index_character.svg"
-                alt="character"
-                width={150}
-                height={175}
+        <Image
+          src="/index_character.svg"
+          alt="character"
+          width={150}
+          height={175}
         />
       </div>
     </main>
-  )
+  );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,20 @@
-import cx from 'classnames'
-import styles from './Button.module.scss';
-import { PropsWithChildren } from 'react';
+import cx from "classnames";
+import styles from "./Button.module.scss";
+import { PropsWithChildren } from "react";
 
 interface Props {
-    className?: string
-    onClick?: React.MouseEventHandler<HTMLButtonElement>
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ className, onClick, children }: PropsWithChildren<Props>) {
+export default function Button({
+  className,
+  onClick,
+  children,
+}: PropsWithChildren<Props>) {
   return (
     <button className={cx(styles.Button, className)} onClick={onClick}>
       {children}
     </button>
-  )
+  );
 }
