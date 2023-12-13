7d288fbaddb8a85caef062da9cdf3f5cb754fbbe
김수빈 <57705512+MOBUMIN@users.noreply.github.com>
2023-12-13 20:34:25 +0900

    feat(page): 반응형 지원, png로 잠시 다운그레이드

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,52 @@
 import Image from "next/image";
-import styles from "./page.module.scss";
 import Button from "@/components/Button";
+
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.main__background}>
-        <Image
-          src="/index_lines.svg"
-          alt="background"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
-        />
+        <span>
+          <Image
+            src="/index_lines.svg"
+            alt="background"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: "100%", height: "auto" }}
+            priority
+          />
+          <div className={styles.main__title}>
+            <span className={styles.main__title__year}>
+              <Image
+                src="/index_title.svg"
+                alt="title"
+                width={265}
+                height={170}
+              />
+              <Image
+                src="/index_0line.svg"
+                alt="line"
+                width={43}
+                height={49}
+                style={{
+                  position: "absolute",
+                  top: "20px",
+                  left: "80px",
+                }}
+              />
+            </span>
+            <p>엠비티아이</p>
+            <Image
+              src="/index_character.png"
+              alt="character"
+              width={150}
+              height={175}
+            />
+          </div>
+        </span>
         <Button>깃허브 로그인하기</Button>
-      </div>
-      <div className={styles.main__body}>
-        <div className={styles.main__title}>
-          <span className={styles.main__title__year}>
-            <Image
-              src="/index_title.svg"
-              alt="title"
-              width={265}
-              height={0}
-              style={{ height: "auto" }}
-            />
-            <Image
-              src="/index_0line.svg"
-              alt="line"
-              width={43}
-              height={0}
-              style={{
-                height: "auto",
-                position: "absolute",
-                top: "20px",
-                left: "80px",
-              }}
-            />
-          </span>
-          <p>엠비티아이</p>
-        </div>
-        <Image
-          src="/index_character.svg"
-          alt="character"
-          width={150}
-          height={175}
-        />
       </div>
     </main>
   );
